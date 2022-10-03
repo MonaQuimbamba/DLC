78a1e3d4920a642a7400258f0870117ac54935da
monaquimbamba <claudio.antonio@etu.unilim.fr>
2022-10-03 13:30:30 +0200

    tp1 almost done

--- TP2/rsa_crt_cle.c
+++ TP2/rsa_crt_cle.c
@@ -5,51 +5,136 @@
 #include "gmp.h"
 
 /*
-crivez une fonction de chiffrement encrypt rsa(c, m, n, e) qui prend en
-entr ́ee les deux  ́el ́ements n et e d’une cl ́e publique RSA ainsi qu’un message
-m ∈ Zn, et qui calcule la valeur du chiffr ́e de m, soit c = me mod n.
- 
- Ecrivez une fonction de d ́echiffrement decrypt rsa(m, c, n, d) qui prend
-en entr ́ee le module public n, l’exposant priv ́e d ainsi qu’un chiffr ́e c ∈ Zn,
-et qui calcule la valeur du d ́echiffr ́e de c, soit m = cd mod n.
- ́Ecrivez un programme qui prend en entr ́ee le nom d’un fichier `a chiffrer et
-le nom d’un fichier de cl ́e RSA, et qui chiffre le contenu du fichier plutˆot qu’un
-entier n’ayant pas de signification  ́evidente. Pour r ́ealiser ce chiffrement, le
-contenu du fichier devra ˆetre trait ́e par blocs d’octets, et chacun de ces blocs
-sera encod ́e en un entier mi ∈ Zn en consid ́erant sa s ́equence d’octets comme
-la repr ́esentation de mi en base 256.
- ́Ecrivez le programme qui r ́ealise l’op ́eration inverse, c’est `a dire qui
-d ́echiffre un fichier chiffr ́e.
- 
- 
+1.1 G ́en ́eration de cl ́e (mode CRT)
+ ́Ecrivez un programme qui prend en entr ́ee deux entiers k et e, et qui
+g ́en`ere une cl ́e RSA (en mode standard) de k bits avec e comme exposant
+public.
+Vous devrez veillez `a ce que le module n soit le produit de deux premiers
+diff ́erents p et q de tailles sensiblement  ́egales, et qu’il soit lui-mˆeme de taille
+exactement k bits.
+Pour permettre le stockage de la cl ́e dans un fichier par redirection de
+la sortie standard, votre programme devra afficher la cl ́e en hexad ́ecimal sur
+plusieurs lignes sous la forme suivante :
+e = 0x......
+n = 0x......
+p = 0x......
+q = 0x......
+dp = 0x......
+dq = 0x......
+ip = 0x........
+Remarque 1 Pour que l’inverse de e modulo φ(n) existe, il faut que e et
+φ(n) soient premiers entre eux.
  * */
-
-
-void encrypt_rsa( mpz_t c, mpz_t m,mpz_t  n,mpz_t  e) 
-{
-        mpz_powm (mpz_t c, mpz_t m, mpz_t e, mpz_t n);
-}
-
-void decrypt_rsa( mpz_t m, mpz_t c,mpz_t  n,mpz_t  d) 
-{
-    mpz_powm (mpz_t m, mpz_t c, mpz_t d, mpz_t n);
-}
 
 
 int main(int argc,char* argv[])
 {
-   
-     mpz_t z_e,z_n,z_d,z_c,z_m;
-     mpz_inits(z_e,z_n,z_d,z_c,z_m;NULL);
-  
+     unsigned int k,e,seed;
+     int isprime;
+     unsigned int chiffre;
+     unsigned int bit_size;
+     mpz_t z_p,z_q,z_e,z_n,rand,z_gcd,z_dp,z_dq,z_ip;
+     mpz_t bord_add;
+     mpz_t phi;
 
-
-     if (argc != 4 ){
-     	printf("Usage : %s <file> <RSA keys> <d/e>  \n", argv[0]);
+     if (argc != 3 ){
+     	printf("Usage : %s k  e  \n", argv[0]);
      	exit(-1);
      }
 
- 
-     mpz_clears(z_e,z_n,z_d,z_c,z_m,NULL);
+     k= atoi(argv[1]);
+     k=k/2;
+     e= atoi(argv[2]);
+   
+   /***
+    ===> 1   // generation de p et q 
+   */
+   
+     mpz_inits(z_p,z_q,z_n,z_e,rand,z_gcd,bord_add,phi,z_dp,z_dq,z_ip,NULL);
+     mpz_set_ui(z_e,e);
+     seed=time(NULL);     
+   /*
+    generer un rand entre 0 et 2^(k-1) - 1
+     et puis ajouter 2^(k)-1
+   */
+   gmp_randstate_t mon_generateur; 
+   gmp_randinit_default(mon_generateur); 
+   gmp_randseed_ui(mon_generateur, seed);
+
+
+while(1){
+
+   int nb_primes=2;
+   while (nb_primes >0)
+   {
+          while(1){
+               mpz_urandomb(rand,mon_generateur,k-1);
+               mpz_ui_pow_ui(bord_add,2,k-1);
+               mpz_add(rand,rand,bord_add);
+               // test primalite
+               isprime = mpz_probab_prime_p(rand, 10);
+               if (isprime ==1 || isprime==2)break;
+          }
+     if(nb_primes==2) {
+          mpz_set(z_p,rand);
+           nb_primes--;
+     }
+     if(nb_primes==1)
+     {
+         mpz_gcd(z_gcd,z_p,rand);
+         if( mpz_cmp_ui(z_gcd,1)==0){
+               mpz_set(z_q,rand);
+                 nb_primes--;  // stop the first loop 
+         }
+     }
+   }
+
+   /***
+          2 =>   Creation du modulo n et du phi de n 
+   ****/
+     mpz_mul(z_n,z_p,z_q);
+     mpz_t tmp_p;
+     mpz_t tmp_q;
+     mpz_init_set(tmp_p,z_p);
+     mpz_init_set(tmp_q,z_q);
+     mpz_sub_ui(tmp_p,tmp_p,1);
+     mpz_sub_ui(tmp_q,tmp_q,1);
+     mpz_mul(phi,tmp_p,tmp_q);
+     mpz_clears(tmp_p,tmp_q,NULL);
+     mpz_gcd(z_gcd,z_e,phi);
+     if( mpz_cmp_ui(z_gcd,1)==0) break;
+}
+  
+    
+    /****
+     *   3 => Calculer le dp and dq and Ip
+     ***/
+     int idp,idq,ip;
+     mpz_t tmpP,tmpQ;
+     mpz_inits(tmpP,tmpQ);
+
+     mpz_sub_ui(tmpP,z_p,1);
+     mpz_sub_ui(tmpQ,z_q,1);
+
+     idp=   mpz_invert(z_dp,z_e,tmpP);
+     idq =  mpz_invert(z_dq,z_e,tmpQ);
+     ip =   mpz_invert(z_ip,z_p,z_q);
+
+    //printf(" inverse p %d  and q %d and ip %d  ",idp,idq,ip );
+
+     mpz_clears(tmpP,tmpQ,NULL);
+     // to save the keys on fille by redirection 
+   
+     gmp_printf("e = %Zx  \n",z_e);
+     gmp_printf("n = %Zx  \n",z_n);
+     gmp_printf("p = %Zx  \n",z_p);
+     gmp_printf("q = %Zx  \n",z_q);
+     gmp_printf("dp = %Zx  \n",z_dp);
+     gmp_printf("dq = %Zx  \n",z_dq);
+     gmp_printf("ip = %Zx  \n",z_ip);
+   
+     gmp_randclear (mon_generateur);
+     mpz_clears(z_p,z_q,z_n,z_e,rand,z_gcd,bord_add,phi,z_dp,z_dq,z_ip,NULL);
+     return 0;
 
 }
--- TP2/sys_rsa_crt.c
+++ TP2/sys_rsa_crt.c
@@ -27,29 +27,84 @@ d ́echiffre un fichier chiffr ́e.
 
 void encrypt_rsa( mpz_t c, mpz_t m,mpz_t  n,mpz_t  e) 
 {
-        mpz_powm (mpz_t c, mpz_t m, mpz_t e, mpz_t n);
+        mpz_powm (c,m,e,n);
 }
 
-void decrypt_rsa( mpz_t m, mpz_t c,mpz_t  n,mpz_t  d) 
+void decrypt_rsa( mpz_t m, mpz_t c,mpz_t  p,mpz_t q,mpz_t  dp,mpz_t dq,mpz_t ip) 
 {
-    mpz_powm (mpz_t m, mpz_t c, mpz_t d, mpz_t n);
+    mpz_t mp,mq,sub1,modq;
+    mpz_inits(mp,mq,sub1,modq,NULL);
+    mpz_powm (mp,c,dp,p);
+    mpz_powm (mq,c,dq,q);
+    // mp + p*((mq-mp)*ip[q])
+    mpz_sub(sub1,mq,mp);
+    mpz_mul(modq,sub1,ip);
+    mpz_powm_ui(m,modq,1,q);
+    mpz_mul(m,m,p);
+    mpz_add(m,m,mp);
+    mpz_clears(mp,mq,sub1,modq,NULL);
 }
 
 
 int main(int argc,char* argv[])
 {
    
-     mpz_t z_e,z_n,z_d,z_c,z_m;
-     mpz_inits(z_e,z_n,z_d,z_c,z_m;NULL);
+     mpz_t z_e,z_n,z_p,z_q,z_dp,z_dq,z_ip,z_c,z_m;
+   
   
-
+     FILE *fp_cipher;
+     FILE *fp_plain;
+     FILE *fp_keys;
+     
 
      if (argc != 4 ){
-     	printf("Usage : %s <file> <RSA keys> <d/e>  \n", argv[0]);
+     	printf("Usage : %s  <-d/e> <file> <RSA keys>  \n", argv[0]);
      	exit(-1);
      }
 
+    char mode= argv[1][0];
+
+   // inits 
+    mpz_inits(z_e,z_n,z_p,z_q,z_dp,z_dq,z_ip,z_c,z_m,NULL);
+   
+    switch (mode)
+    {
+        case 'e' :
+           
+            fp_cipher = fopen(argv[2],"r");
+            gmp_fscanf(fp_cipher, "%*c%*c%*c%*c%Zx", z_m);
+            fp_keys = fopen(argv[3],"r");
+            gmp_fscanf(fp_keys, "%*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx",z_e, z_n ,z_p,z_q,z_dp,z_dq,z_ip);
+            
+            encrypt_rsa( z_c, z_m,z_n,z_e); 
+            gmp_printf("c = %Zx\n",z_c);
+            fclose(fp_cipher);
+            fclose(fp_keys);
+        break;
+
+        case 'd':
+
+            fp_plain = fopen(argv[2],"r");
+            gmp_fscanf(fp_plain, "%*c%*c%*c%*c%Zx", z_c);
+
+            fp_keys = fopen(argv[3],"r");
+            gmp_fscanf(fp_keys, "%*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx %*c%*c%*c%*c%Zx",z_e, z_n ,z_p,z_q,z_dp,z_dq,z_ip);
+            decrypt_rsa(z_m, z_c,z_p,z_q,z_dp,z_dq,z_ip);
+            gmp_printf("m = %Zx\n",z_m);
+            /*gmp_printf("e= %Zx\n",z_e);
+            gmp_printf("n= %Zx\n",z_n);
+            gmp_printf("p= %Zx\n",z_p);
+            gmp_printf("q= %Zx\n",z_q);
+            gmp_printf("dp= %Zx\n",z_dp);
+            gmp_printf("dq= %Zx\n",z_dq);
+            gmp_printf("ip= %Zx\n",z_ip);*/
+            fclose(fp_plain);
+            fclose(fp_keys);
  
-     mpz_clears(z_e,z_n,z_d,z_c,z_m,NULL);
+        break;
+    }
+
+ 
+     mpz_clears(z_e,z_n,z_p,z_q,z_dp,z_dq,z_ip,z_c,z_m,NULL);
 
 }
