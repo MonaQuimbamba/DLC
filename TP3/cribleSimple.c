9ee55ea06c3ec043e86b2a118f101ab6c1b2e5a8
monaquimbamba <claudio.antonio@etu.unilim.fr>
2022-10-20 20:43:19 +0200

    almost almost tp3  done

--- TP3/cribleSimple.c
+++ TP3/cribleSimple.c
@@ -120,7 +120,7 @@ bool  miller_rabin(mpz_t n , __uint64_t t,mpz_t base_output){
 
 
 
-bool basic_sieve(uint16_t b , uint16_t k , uint16_t t,mpz_t *my_array,mpz_t output, gmp_randstate_t mon_generateur ){
+bool makePrime(uint16_t b , uint16_t k , uint16_t t,mpz_t *my_array,mpz_t output, gmp_randstate_t mon_generateur ){
 
 
 
@@ -143,6 +143,7 @@ bool basic_sieve(uint16_t b , uint16_t k , uint16_t t,mpz_t *my_array,mpz_t outp
                         {
                            res= true;
                            //gmp_printf(" q est premier = %Zu  dans la base  %Zu  \n", q , base );
+                            gmp_printf("%Zu  %ld\n",my_array[i],cputime());
                         }
                         else{
                             mpz_clears(bord_add,base,q,NULL);
@@ -168,11 +169,11 @@ void find_r_primes(int b , int k , int t, int r,mpz_t *my_array,gmp_randstate_t 
         mpz_t prime;
         mpz_init(prime);
          while( r > 0){
-            if(basic_sieve(b,k,t,my_array,prime,mon_generateur)==1)
+            if(makePrime(b,k,t,my_array,prime,mon_generateur)==1)
             {
 
                 r--;
-                   gmp_printf("%Zu  %ld\n",prime,cputime());
+                  // gmp_printf("%Zu  %ld\n",prime,cputime());
                   int bit_size = mpz_sizeinbase(prime, 2);
                    //printf("%u bits \n", bit_size);
             }
@@ -183,7 +184,9 @@ void find_r_primes(int b , int k , int t, int r,mpz_t *my_array,gmp_randstate_t 
 
 
 void cribleOpt(mpz_t* primes,uint64_t k){
-    int  i, j;  
+    int  i;
+    mpz_t j,power,minus;  
+    mpz_inits(j,power,minus,NULL);
     int limit = sqrt(k);  
   
     for(i = 0; i < k; i++)  mpz_init_set_ui(primes[i],i+1);
@@ -192,46 +195,27 @@ void cribleOpt(mpz_t* primes,uint64_t k){
     {  
         if(mpz_cmp_ui(primes[i],0) != 0)  
         {  
-            for(j = pow(num[i], 2); j <= N; j = j + num[i])  
-            {  
-                num[j - 1] = 0;  
-            }  
+            mpz_pow_ui(power,primes[i],2);
+            mpz_set(j,power);
+            while(mpz_cmp_ui(j,k)<0){
+            
+                 mpz_sub_ui(minus,j,1);
+                int index=mpz_get_ui(minus);  
+                mpz_set_ui(primes[index],0);
+                mpz_add(j,j,primes[i]);
+            }
+           
         }  
   
     }  
 
 }
-int main(int argc,char* argv[])
-{
 
-    gmp_randstate_t mon_generateur; 
-    gmp_randinit_default(mon_generateur); 
-    gmp_randseed_ui(mon_generateur, time(NULL));
+void criblesimple(mpz_t * primes,uint64_t k){
 
-
-
-    if (argc != 5 ){
-     	printf("Usage : %s b  k  t  r   \n", argv[0]);
-     	exit(-1);
-     }
-
-    uint16_t b= atoi(argv[1]);
-    uint16_t k= atoi(argv[2]);
-    uint16_t t= atoi(argv[3]);
-    uint16_t r= atoi(argv[4]);
-
-    mpz_t *primes;
-    primes = malloc(sizeof(mpz_t) * k-1);
-
-    mpz_t rand,base,output,testBoucle,mult,j,minus;
-    mpz_inits(rand,base,output,testBoucle,mult,j,minus,NULL);
-
-  
-   
-
-
-   
-    int  i;  
+    mpz_t testBoucle,mult,j,minus;
+    mpz_inits(testBoucle,mult,j,minus,NULL);
+     int  i;  
     for(i = 0; i < k; i++)  mpz_init_set_ui(primes[i],i+1);
 
     mpz_mul(mult,primes[1],primes[1]);
@@ -259,19 +243,39 @@ int main(int argc,char* argv[])
         mpz_set(testBoucle,mult);
     }  
   
-    
 
-
-    //for(int i =1 ; i < k; i++) gmp_printf(" %Zu ",primes[i]);
-  
-    //basic_sieve(b,k,t,primes,output,mon_generateur);
-     find_r_primes(b,k,t,r,primes,mon_generateur);
-    gmp_randclear(mon_generateur); 
-
-    //printf(" time %ld",cputime());
-    return 0;
-     
 }
 
 
-//https://technotip.com/9436/prime-numbers-using-sieve-of-eratosthenes-c-program/
+
+int main(int argc,char* argv[])
+{
+
+    gmp_randstate_t mon_generateur; 
+    gmp_randinit_default(mon_generateur); 
+    gmp_randseed_ui(mon_generateur, time(NULL));
+
+
+
+    if (argc != 5 ){
+     	printf("Usage : %s b  k  t  r   \n", argv[0]);
+     	exit(-1);
+     }
+
+    uint16_t b= atoi(argv[1]);
+    uint16_t k= atoi(argv[2]);
+    uint16_t t= atoi(argv[3]);
+    uint16_t r= atoi(argv[4]);
+
+    mpz_t *primes;
+    primes = malloc(sizeof(mpz_t) * k-1);
+
+ 
+    criblesimple(primes,k);
+    //for(int i =1 ; i < k; i++) gmp_printf(" %Zu ",primes[i]);
+    //makePrime(b,k,t,primes,output,mon_generateur);
+    find_r_primes(b,k,t,r,primes,mon_generateur);
+    gmp_randclear(mon_generateur); 
+    return 0;
+     
+}
