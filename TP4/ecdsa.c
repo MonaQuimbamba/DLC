e7c17d7c57fc1ea368c5cdac861b9b475663fb90
monaquimbamba <claudio.antonio@etu.unilim.fr>
2022-10-24 09:38:15 +0200

    doin tp4

--- TP4/ecdsa.c
+++ TP4/ecdsa.c
@@ -47,13 +47,16 @@ int sur_courbe(point P , mpz_t p,mpz_t a , mpz_t b){
 
 
 }
+
+
+
+
 /**
 *qui calcule le point R = P + Q. Votre fonction devra v ́erifier que les
 points P et Q appartiennent `a la courbe et v ́erifient les conditions pour
 pouvoir ˆetre additionn ́es. Elle renverra 0 si l’addition est possible et -1
 sinon
 *
-
 λ = yQ −yP/xQ −xP
 xR = λ2 − xP − xQ
 yR = λ(xP − xR ) − y
@@ -61,25 +64,32 @@ yR = λ(xP − xR ) − y
 int addition(point *R, point P, point Q,mpz_t p, mpz_t a, mpz_t b){
 
 
-        mpz_t p_x,p_y;
-        mpz_inits(p_x,p_y,NULL);
+        mpz_t p_x,p_y, Y_minus,X_minus,lambda,X_r,Y_r,double_lambda,tmp;
+        mpz_inits(p_x,p_y,Y_minus,X_minus,lambda,X_r,Y_r,double_lambda,tmp,NULL);
         if(sur_courbe(P,p,a,b)==1 && sur_courbe(Q,p,a,b)==1 && mpz_cmpabs(P.x,Q.x)!=0 && mpz_cmpabs(P.y,Q.y)!=0  ){
 
-            mpz_add(p_x,P.x,Q.x);
-            mpz_add(p_y,P.y,Q.y);
+            mpz_sub(Y_minus,Q.y,P.y);
+            mpz_sub(X_minus,Q.x,P.x);
 
-            mpz_set(R->x,p_x);
-            mpz_set(R->y,p_y);
+            if(mpz_invert(lambda,Y_minus,X_minus)!=0){
 
-            mpz_clears(p_x,p_y,NULL);
-            return 1;
+                    mpz_pow_ui(double_lambda,lambda,2);
+                    mpz_sub(X_r,double_lambda,P.x);
+                    mpz_sub(X_r,X_r,Q.x);
 
+                    mpz_sub(tmp,P.x,X_r);
+                    mpz_mul(tmp,tmp,lambda);
+                    mpz_sub(Y_r,tmp,P.y);
+
+                    mpz_set(R->x,X_r);
+                    mpz_set(R->y,Y_r);
+                     return 0;
+            }
         }
+        mpz_clears(p_x,p_y,Y_minus,X_minus,lambda,X_r,Y_r,double_lambda,tmp,NULL);
+        return -1;
 
-
-        mpz_clears(p_x,p_y,NULL);
-        return 0;
-    
+ 
 
    
 }
@@ -101,19 +111,124 @@ yR = λ(xP − xR ) − y
 */
 int doublement(point *R, point P,mpz_t p, mpz_t a, mpz_t b){
 
+
+     mpz_t p_x,p_y, lambda_up,lambda_down,lambda,X_r,Y_r,double_lambda,tmp,double_XP;
+     mpz_inits(p_x,p_y,lambda_up,lambda_down,lambda,X_r,Y_r,double_lambda,tmp,double_XP,NULL);
     if(sur_courbe(P,p,a,b)==1){
 
-        mpz_mul_ui(R->x,P.x,2);
-        mpz_mul_ui(R->y,P.x,2);
+
+        mpz_pow_ui(double_XP,P.x,2);
+        mpz_mul_ui(lambda_up,double_XP,3);
+        mpz_add(lambda,lambda_up,a);
+
+        mpz_mul_ui(lambda_down,P.y,2);
+
+        if(mpz_invert(lambda,lambda_up,lambda_down)!=0){
+
+            mpz_pow_ui(double_lambda,lambda,2);
+            mpz_mul_ui(X_r,P.x,2);
+            mpz_sub(X_r,double_lambda,X_r);
+
+            mpz_sub(tmp,P.x,X_r);
+            mpz_mul(tmp,lambda,tmp);
+            mpz_sub(Y_r,tmp,P.y);
+              
+        
+           mpz_set(R->x,X_r);
+           mpz_set(R->y,Y_r);
+
+             mpz_clears(p_x,p_y,lambda_up,lambda_down,lambda,X_r,Y_r,double_lambda,tmp,double_XP,NULL);
+            return 0;
+        }
+    }
+    
+    mpz_clears(p_x,p_y,lambda_up,lambda_down,lambda,X_r,Y_r,double_lambda,tmp,double_XP,NULL);
+    return -1;
+
+} 
+
+
+
+
+/***
+ *  d=|k|
+ *  Q= k*P
+ *   pour d-1 à 0:
+ *       Q <- Q*Q // 2*Q
+ *          if di==1:
+ *              Q<-Q*P // Q+ P
+ *   return Q
+ */
+void addition_scalaire(point *R, point P, mpz_t k, mpz_t p, mpz_t a, mpz_t b,mpz_t output){
+
+
+    unsigned int bit_size;
+    mpz_t z_Q;
+    mpz_init(z_Q);
+
+    bit_size = mpz_sizeinbase(k, 2);
+    mpz_mul(z_Q,z_Q,k);
+    for (int i = bit_size - 1; i >= 0; i--)
+    {
+        //mpz_add(z_Q,z_Q,z_Q); // 2*Q
+        if(doublement(R,P,p,a,b)==0){
+            if (mpz_tstbit(k, i) == 1)
+            {
+                //mpz_add(z_Q,z_Q,P); //  Q+ P
+                 //addition(R,P,Q,p,a,b);
+            }
+        }
+        else{
+            printf(" erreur dans le doublement ");
+        }
+        
+    }
+
+    mpz_set(output,z_Q);
+}
+/**
+ *   R = k · P => k · P = P + P + . . . + P
+ * 
+*/
+int multiple(point *R, point P, mpz_t k, mpz_t p, mpz_t a, mpz_t b) {
+
+
+    if(sur_courbe(P,p,a,b)==1){
+
+        //addition_scalaire(P.x,k,R->x);
+        //addition_scalaire(P.y,k,R->y);
+        return 0;
+    }
+    return -1;
+}
+
+
+void testPoint(point G,mpz_t k){
+
+    point P;
+    mpz_t p,a,b;
+    mpz_inits(P.x,P.y,p,a,b,NULL);
+    FILE *fp;
+    fp = fopen("parametres.txt","r");
+    
+    gmp_fscanf(fp, "%*c%*c%*c%*c%Zu\n %*c%*c%*c%*c%Zu\n %*c%*c%*c%*c%Zu\n %*c%*c%*c%*c%Zx", P.x,P.y,a,b);
+    /*
+    gmp_printf(" %Zu \n",P.x);
+    gmp_printf(" %Zu \n",P.y);
+    gmp_printf(" %Zu \n",a);
+    gmp_printf(" %Zx ",b);*/
+
+    if (mpz_odd_p(k)==0){
+
+    }
+    else{
 
     }
 
-    return 0;
+}
 
-} 
 int main(int argc, char const *argv[])
 {
-    /* code */
 
 
     mpz_t p ,b,a;
@@ -133,9 +248,12 @@ int main(int argc, char const *argv[])
     mpz_set_ui(a,3);
 
 
-    int sur_c = addition(&R,P,Q,p,a,b);
+    int addtest = addition(&R,P,Q,p,a,b);
+    int doubletest = doublement(&R,P,p,a,b);
 
-    printf(" le point %d ",sur_c);
+    //printf(" le point %d ",doubletest);
+
+    testPoint(Q,a);
 
     return 0;
 }
